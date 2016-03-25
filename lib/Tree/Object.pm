11ac9ed84073b5473ebb8a8cd016587df30a4e8f
perlancar (@netbook-zenbook) <perlancar@gmail.com>
2016-03-25 09:26:53 +0700

    Forgot to include the new, now-empty Tree::Module

---
+++ lib/Tree/Object.pm
@@ -0,0 +1,17 @@
+package Tree::Object;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Generic tree objects
+
+=head1 DESCRIPTION
+
+This distribution provides several implementation of tree object classes (and
+class generators) which you can use directly or as a base class.
+as a base class.
+
+Provided classes:
+
+# INSERT_MODULES_LIST -Tree::Object
